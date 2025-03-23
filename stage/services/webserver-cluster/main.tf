6bb6acec77d5d4425730a554cba04cce47b3f12f
k-abe <f.kaebnet.o.cus@gmail.com>
2025-03-24 00:25:52 +0900

    Initial Commit

---
+++ stage/services/webserver-cluster/main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# TerraformのstateファイルをS3に保存する
+terraform {
+  backend "s3" {
+    key            = "stage/services/webserver-cluster/terraform.tfstate"
+    bucket         = "terraform-up-and-running-state-20250309"
+    region         = "ap-northeast-1"
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+module "webserver_cluster" {
+  source = "github.com/kenteru59/practice4_module_version_manage//services/webserver-cluster?ref=v0.0.2"
+
+  cluster_name           = "webserver-stage"
+  db_remote_state_bucket = "terraform-up-and-running-state-20250309"
+  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 10
+  env_name      = "stage"
+}
+
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  type              = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
+
+  from_port   = 12345
+  to_port     = 12345
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+output "asg_name" {
+  value       = module.webserver_cluster.alb_dns_name
+  description = "The domain name of the load balancer"
+}
